ae6885c609ca268cc4e38b5c5577e43179674c7c
Amir Nouri <Ru13y051@gmail.com>
2021-12-20 13:01:09 +0330

    Create Slider Withe W3School

--- tss.js
+++ tss.js
@@ -127,3 +127,33 @@ function switchTheme(event) {
 }
 //Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+//Slider
+var slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].setAttribute('style', "display:none");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].setAttribute('style', "display:block");
+    dots[slideIndex - 1].className += " active";
+}
--- tss.ts
+++ tss.ts
@@ -190,3 +190,34 @@ function switchTheme(event){
 //Event Listener
 toggleSwitch.addEventListener('change',switchTheme);
 
+
+//Slider
+
+let slideIndex:number = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n:number) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n:number) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n:number) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].setAttribute('style',"display:none")
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].setAttribute('style',"display:block")
+  dots[slideIndex-1].className += " active";
+}
